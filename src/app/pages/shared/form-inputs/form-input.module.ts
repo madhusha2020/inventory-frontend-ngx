ac418afe24485a227ecd7fbfd8b46563eb8c815d
malindu-hdps <malindu@interblocks.com>
2020-10-26 07:55:32 +0530

    Input and Button theme set via nebular tags

--- src/app/pages/shared/form-inputs/form-input.module.ts
+++ src/app/pages/shared/form-inputs/form-input.module.ts
@@ -4,6 +4,7 @@ import {InputSelectDefaultComponent} from './input-select-default/input-select-d
 import { InputDefaultComponent } from './input-default/input-default.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {FormValidationModule} from '../form-validations/form-validation.module';
+import {NbInputModule} from '@nebular/theme';
 
 
 
@@ -18,7 +19,8 @@ import {FormValidationModule} from '../form-validations/form-validation.module';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    FormValidationModule
+    FormValidationModule,
+    NbInputModule
   ]
 })
 export class FormInputModule { }
--- src/app/service/auth/auth.module.ts
+++ src/app/service/auth/auth.module.ts
@@ -7,27 +7,35 @@ import {AuthRoutingModule} from './auth-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import {AuthGuardService} from './auth-guard.service';
 import {TokenService} from './token.service';
-import {NbAlertModule, NbCardModule, NbCheckboxModule, NbInputModule, NbSpinnerModule} from '@nebular/theme';
+import {
+  NbAlertModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbInputModule,
+  NbSpinnerModule
+} from '@nebular/theme';
 import { LoginComponent } from './login/login.component';
 import {NbAuthModule} from '@nebular/auth';
 import {FormInputModule} from '../../pages/shared/form-inputs/form-input.module';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        RouterModule,
-        AuthRoutingModule,
-        HttpClientModule,
-        NbCardModule,
-        NbAlertModule,
-        NbInputModule,
-        NbCheckboxModule,
-        NbAuthModule,
-        ReactiveFormsModule,
-        FormInputModule,
-        NbSpinnerModule,
-    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    AuthRoutingModule,
+    HttpClientModule,
+    NbCardModule,
+    NbAlertModule,
+    NbInputModule,
+    NbCheckboxModule,
+    NbAuthModule,
+    ReactiveFormsModule,
+    FormInputModule,
+    NbSpinnerModule,
+    NbButtonModule,
+  ],
   declarations: [
   LoginComponent
   ],
