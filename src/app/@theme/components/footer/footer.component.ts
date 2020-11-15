cbf49fbda9dee3c46f5bf8d8ca52880fded081e1
malindu-hdps <malindu@interblocks.com>
2020-11-15 19:57:15 +0530

    Footer Change

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,12 +5,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">&copy; Copyright 2020 Water Solution (Pvt) Ltd.</span>
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
+<!--    <div class="socials">-->
+<!--      <a href="#" target="_blank" class="ion ion-social-github"></a>-->
+<!--      <a href="#" target="_blank" class="ion ion-social-facebook"></a>-->
+<!--      <a href="#" target="_blank" class="ion ion-social-twitter"></a>-->
+<!--      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>-->
+<!--    </div>-->
   `,
 })
 export class FooterComponent {
