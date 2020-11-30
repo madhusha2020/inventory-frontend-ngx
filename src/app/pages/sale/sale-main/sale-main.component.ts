113d1f39adf0de7b55f465f6f8fc5f321a64d8fa
malindu-hdps <malindu@interblocks.com>
2020-11-30 16:09:07 +0530

    Bug Fixed

--- src/app/pages/sale/sale-main/sale-main.component.ts
+++ src/app/pages/sale/sale-main/sale-main.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Sale, SaleControllerService} from '../../../service/rest';
+import {LocalDataSource} from 'ng2-smart-table';
+import {NbSearchService} from '@nebular/theme';
+import {TokenService} from '../../../service/auth/token.service';
+import {Router} from '@angular/router';
+import {ServiceUtil} from '../../../service/util/service-util';
 
 @Component({
   selector: 'ngx-sale-main',
@@ -7,9 +13,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaleMainComponent implements OnInit {
 
-  constructor() { }
+  offset = 0;
+  limit = 100;
 
-  ngOnInit(): void {
+  saleList: Array<Sale> = [];
+
+  settings = {
+    hideSubHeader: true,
+    actions: false,
+    columns: {
+      id: {
+        title: 'Sale#',
+        type: 'number',
+      },
+      name: {
+        title: 'Customer Name',
+        type: 'string',
+      },
+      deliveryaddress: {
+        title: 'Delivery Address',
+        type: 'string',
+      },
+      totalValue: {
+        title: 'Total Amount',
+        type: 'string',
+      },
+      discountValue: {
+        title: 'Discount',
+        type: 'string',
+      },
+      contact1: {
+        title: 'Contact',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      statusDescription: {
+        title: 'Status',
+        type: 'string',
+      },
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private saleControllerService: SaleControllerService,
+              private searchService: NbSearchService,
+              private tokenService: TokenService,
+              private router: Router) {
+
+    this.searchService.onSearchSubmit()
+      .subscribe((data: any) => {
+        console.log('Search', data.term);
+        this.source.setFilter([
+          {
+            field: 'name',
+            search: data.term
+          }
+        ], false);
+      });
   }
 
+  ngOnInit(): void {
+    this.fetchCustomerSale();
+  }
+
+  fetchCustomerSale() {
+    this.saleControllerService.getAllSalesUsingGET().subscribe(response => {
+      console.log('All Sales Data :', response);
+      response.saleList.forEach(sale => {
+        sale.statusDescription = ServiceUtil.getStatusDescription(sale.status);
+        sale.totalValue = sale.total.toLocaleString('en-US', {style: 'currency', currency: 'LKR'});
+        sale.discountValue = sale.discount.toLocaleString('en-US', {style: 'currency', currency: 'LKR'});
+        this.saleList.push(sale);
+      });
+      this.source.load(this.saleList);
+    });
+  }
+
+  onUserRowSelect(event): void {
+    console.log(event);
+    this.router.navigate(['/pages/sale/view'], {queryParams: {id: event.data.id}});
+  }
+
+  resetFilter(): void {
+    this.source.setFilter([]);
+  }
 }
--- src/app/pages/sale/sale.module.ts
+++ src/app/pages/sale/sale.module.ts
@@ -5,13 +5,20 @@ import { SaleRoutingModule } from './sale-routing.module';
 import {SaleComponent} from './sale.component';
 import { SaleMainComponent } from './sale-main/sale-main.component';
 import { SaleViewComponent } from './sale-view/sale-view.component';
+import {NbButtonModule, NbCardModule, NbSearchModule, NbTooltipModule} from '@nebular/theme';
+import {Ng2SmartTableModule} from 'ng2-smart-table';
 
 
 @NgModule({
   declarations: [SaleComponent, SaleMainComponent, SaleViewComponent],
   imports: [
     CommonModule,
-    SaleRoutingModule
+    SaleRoutingModule,
+    NbCardModule,
+    NbSearchModule,
+    Ng2SmartTableModule,
+    NbButtonModule,
+    NbTooltipModule
   ]
 })
 export class SaleModule { }
