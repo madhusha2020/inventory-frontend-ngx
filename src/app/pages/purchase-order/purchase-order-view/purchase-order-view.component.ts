bbca424b47381d3c71e47edcb17bc227d3261bec
malindu-hdps <malindu@interblocks.com>
2021-01-16 20:56:18 +0530

    Purchase Order View UI

--- src/app/pages/purchase-order/purchase-order-view/purchase-order-view.component.ts
+++ src/app/pages/purchase-order/purchase-order-view/purchase-order-view.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PurchaseOrder, PurchaseOrderControllerService} from '../../../service/rest';
+import {TokenService} from '../../../service/auth/token.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'ngx-purchase-order-view',
@@ -7,9 +11,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchaseOrderViewComponent implements OnInit {
 
-  constructor() { }
+  purchaseOrderId: string;
+  purchaseOrder: PurchaseOrder = {};
 
-  ngOnInit(): void {
+  constructor(private purchaseOrderControllerService: PurchaseOrderControllerService,
+              private tokenService: TokenService,
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+        if (params.id) {
+          this.purchaseOrderId = params.id;
+          this.getOrder(params.id);
+        } else {
+          Swal.fire('Error', 'Purchase Order not found', 'error');
+        }
+      }
+    );
+  }
+
+  getOrder(id: string) {
+    this.purchaseOrderControllerService.getPurchaseOrderByIdUsingGET(id).subscribe(response => {
+      console.log('OrderItems response :', response);
+      this.purchaseOrder = response;
+    });
+  }
+
+  approve() {
+    console.log('Approve Purchase Order');
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'Approve Purchase Order : {0}'.replace('{0}', this.purchaseOrderId),
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        this.purchaseOrderControllerService.approvePurchaseOrderUsingPUT({
+          id: Number(this.purchaseOrderId),
+          userId: this.tokenService.getUserName()
+        }).subscribe(response => {
+          console.log('Purchase Order Approved :', response);
+          Swal.fire('Success', 'Purchase Order approved successfully', 'success').then(ok => {
+            this.router.navigate(['/pages/purchase-order/main']);
+          });
+        });
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        // Canceled
+      }
+    });
+  }
+
+  suspend() {
+    console.log('Reject Purchase Order');
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'Reject Purchase Order : {0}'.replace('{0}', this.purchaseOrderId),
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        this.purchaseOrderControllerService.rejectPurchaseOrderUsingPUT({
+          id: Number(this.purchaseOrderId),
+          userId: this.tokenService.getUserName()
+        }).subscribe(response => {
+          console.log('Purchase Order Rejected :', response);
+          Swal.fire('Success', 'Purchase Order rejected successfully', 'success').then(ok => {
+            this.router.navigate(['/pages/purchase-order/main']);
+          });
+        });
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        // Canceled
+      }
+    });
+  }
 }
+
--- src/app/pages/purchase-order/purchase-order.module.ts
+++ src/app/pages/purchase-order/purchase-order.module.ts
@@ -6,7 +6,14 @@ import {PurchaseOrderComponent} from './purchase-order.component';
 import { PurchaseOrderCreateComponent } from './purchase-order-create/purchase-order-create.component';
 import { PurchaseOrderMainComponent } from './purchase-order-main/purchase-order-main.component';
 import { PurchaseOrderViewComponent } from './purchase-order-view/purchase-order-view.component';
-import {NbButtonModule, NbCardModule, NbSearchModule, NbStepperModule, NbTooltipModule} from '@nebular/theme';
+import {
+  NbButtonModule,
+  NbCardModule,
+  NbSearchModule,
+  NbStepperModule,
+  NbTabsetModule,
+  NbTooltipModule
+} from '@nebular/theme';
 import {Ng2SmartTableModule} from 'ng2-smart-table';
 import {ReactiveFormsModule} from '@angular/forms';
 import {FormInputModule} from '../shared/form-inputs/form-input.module';
@@ -26,7 +33,8 @@ import {ItemModule} from '../item/item.module';
     NbStepperModule,
     ReactiveFormsModule,
     FormInputModule,
-    ItemModule
+    ItemModule,
+    NbTabsetModule
   ]
 })
 export class PurchaseOrderModule { }
